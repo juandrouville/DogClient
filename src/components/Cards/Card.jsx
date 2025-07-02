a78e999088fa0adb1443388921ee017d342d5ea6
juandrouville <jadrouville@gmail.com>
2025-07-01 22:14:06 -0300

    change visual style

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -12,19 +12,20 @@ function Card({id,name,img,temperaments}){
     }
     return (
         <div className="card">
-            <Link to= {`/details/${id}`} >
-                <img src={img} alt=""/>
+            <Link to= {`/details/${id}`} style={{textDecoration:'none'}} >
+                <img src={img} alt="" className='cardImage'/>
+                <h3 className='nameDog'>{name}</h3>
+                <div className="temperaments">
+                    {temperaments && temperaments.map(ele => 
+                    <Link to={`/temperament/${ele.name}`} style={{textDecoration:'none'}}>
+                    <h1 className='nameTemperament'>{ele.name}</h1>
+                    </Link>
+                    )}
+                </div>
             </Link>
-            <h3>{name}</h3>
-            <div className="temperaments">
-                {temperaments && temperaments.map(ele => 
-                <Link to={`/temperament/${ele.name}`} className="link">
-                <h5>{ele.name}</h5>
-                </Link>
-                )}
-            </div>
         </div>
     );
 };
 
 export default Card;
+            
